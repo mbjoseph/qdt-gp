84453cca906d32514be9d08c170fffd31f5f60af
mbjoseph <maxwellbjoseph@gmail.com>
2015-11-16 14:04:34 -0700

    removing unneeded jitter terms on the diagonal

--- R/sim.R
+++ R/sim.R
@@ -4,30 +4,30 @@ library(scales)
 
 ## Univariate inputs ---------------------------
 n <- 1000
-x <- sort(runif(n, -10, 10))
+x <- sort(runif(n, -6, 10))
 
 # squared exponential --------------------------
-l <- 10  # range parameter
+l <- 1  # range parameter
 dmat <- as.matrix(dist(x))
-C <- exp(-dmat^2 / 2 * l^2) + diag(rep(1E-6, n))
+C <- .007 * exp(-dmat^2 / 2 * l^2) + diag(rep(1E-6, n))
 
 # z %*% L produces multivariate normal draws from MVN(0, Sigma), 
 # where L %*% t(L) = Sigma. i.e., L is a cholesky decomposition of Sigma
 # and z ~ normal(0, 1)
 z <- rnorm(n)
-y <- z %*% chol(C + d)
-plot(x, y, type='l')
+y <- z %*% base::chol(C)
+plot(exp(x), exp(y), type='l')
 
 # or, built into a linear model
 alpha <- -1
 beta <- .5
-y <- alpha + beta * x + z %*% chol(C + d)
+y <- alpha + beta * x + z %*% chol(C)
 plot(x, y, type='l')
 
 # OU covariance function (more jaggety) --------------
 C <- exp(-dmat / l)
 z <- rnorm(n)
-y <- z %*% chol(C + d)
+y <- z %*% chol(C)
 plot(x, y, type='l')
 
 
